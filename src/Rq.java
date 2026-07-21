c52b1510c3948c0ee0cb2b95fda48054ec65bdb7
songfox00 <songfox00@gmail.com>
2026-07-21 11:45:11 +0900

    getParam 메서드 구현

--- src/Rq.java
+++ src/Rq.java
@@ -1,14 +1,44 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Rq {
 
     private String cmd;
+    private String actionName;
+    private Map<String, String> paramMap = new HashMap<>();
 
     public Rq(String cmd) {
         this.cmd=cmd;
+
+        //목록?keywordType=content&keyword=과거
+        String[] cmdBits = cmd.split("\\?");
+        actionName=cmdBits[0];
+
+        String params = cmdBits.length > 1 ? cmdBits[1]: "";
+
+        if(params.equals(""))
+            return;
+
+        String[] paramBits = params.split("&");
+
+        for(String param : paramBits){
+            String[] keyValue = param.split("=");
+
+            if(keyValue.length<2)
+                continue;
+
+            paramMap.put(keyValue[0], keyValue[1]);
+        }
     }
 
     public String getActionName(){
-        String[] cmdBits = cmd.split("\\?");
 
-        return cmdBits[0];
+        return actionName;
+    }
+
+    public String getParam(String key, String defaultValue){
+
+        return paramMap.getOrDefault(key,defaultValue);
+
     }
 }
--- src/RqTest.java
+++ src/RqTest.java
@@ -1,6 +1,56 @@
 public class RqTest {
+
     public static void main(String[] args){
 
+        testActionName();
+//        testGetParam();
+//        testGetParamEx();
+//        testGetParamEx2();
+//        testGetParamEx3();
+    }
+
+    public static void testGetParamEx3(){
+        Rq rq = new Rq("목록?searchKeywrod&keyword=kkk");
+        String searchKeyword = rq.getParam("searchKeyword", ""); // "영광"
+        String keyword = rq.getParam("keyword", ""); // "영광"
+
+        System.out.println(searchKeyword);
+        System.out.println(keyword);
+
+    }
+
+    public static void testGetParamEx2(){
+        Rq rq = new Rq("목록?searchKeywrod=");
+        String searchKeyword = rq.getParam("searchKeyword", ""); // "영광"
+
+        System.out.println(searchKeyword); //
+    }
+
+    public static void testGetParamEx(){
+        Rq rq = new Rq("목록?");
+        String searchKeyword = rq.getParam("searchKeyword", ""); // "영광"
+
+        System.out.println(searchKeyword); //
+    }
+
+    public static void testGetParam(){
+        Rq rq = new Rq("목록?searchKeyword=영광");
+        String searchKeyword = rq.getParam("searchKeyword", ""); // "영광"
+
+        System.out.println(searchKeyword); //영광
+
+        Rq rq2 = new Rq("목록?keywordType=content");
+        String keywordType = rq2.getParam("keywordType", ""); // "영광"
+
+        System.out.println(keywordType); //content
+
+        Rq rq3 = new Rq("목록?keywordType=author");
+        String keywordType2 = rq3.getParam("keywordType", ""); // "영광"
+
+        System.out.println(keywordType2); //content
+;    }
+
+    public static void testActionName(){
         Rq rq1 = new Rq("삭제?id=1");
         String action = rq1.getActionName();
 
